dc2f9329cc17c11a84be3f339bc1d32f86173cfa
Emmanuel López Ortiz <81281475+liquidlevels@users.noreply.github.com>
2024-07-10 13:25:59 -0700

    Delete exercism directory

--- exercism/file.js
+++
@@ -1,5 +0,0 @@
-export const MY_VALUE = 10
-
-export function sum(number1, number2){
-    return number1 + number2
-}
--- exercism/importFile.js
+++
@@ -1,3 +0,0 @@
-import {MY_VALUE, sum} from './file.js';
-
-console.log(sum(20,21))
